954283bd52df9612947c562b8789a0fd91d614ff
General-Servelat <diman.ua68@gmail.com>
2022-01-12 18:09:35 +0200

    almost finished

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const countTable = document.querySelector("#count-table")
+
 function extract(items, arr) {
     items.forEach(item=> {
         const elements = [...item.childNodes]
@@ -10,6 +10,14 @@ function extract(items, arr) {
 function showForm(form) {
     formContainer.classList.add("active")
     form.classList.add("active")
+}
+function fastCreate(parent, element, content, className) {
+    const newElement = document.createElement(element)
+    newElement.innerHTML = content
+    if(className !== undefined) {
+        newElement.classList.add(className)
+    }
+    parent.appendChild(newElement)
 }
 function notesCounter() {
     const items = countTable.querySelectorAll(".table-item")
@@ -26,31 +34,31 @@ function notesCounter() {
     for(let elem of newSet) {
         const div = document.createElement("div")
         div.classList.add("table-item")
-        const category = document.createElement("p")
-        category.innerHTML = elem
-        div.appendChild(category)
-        const activeNotes = document.createElement("p")
-        activeNotes.innerHTML = massActive.filter(item => item === elem).length
-        div.appendChild(activeNotes)
-        const archiveNotes = document.createElement("p")
-        archiveNotes.innerHTML = massArchive.filter(item => item === elem).length
-        div.appendChild(archiveNotes)
+        const auxArray=[
+            elem, 
+            massActive.filter(item => item === elem).length,
+            massArchive.filter(item => item === elem).length,
+        ]
+        auxArray.forEach(item=> {
+            fastCreate(div, "p", item)
+        })
         countTable.appendChild(div)
-    }  
+    } 
 }
 function itemControls(e,table, oppositeTable) {
         const target = e.target.classList[0]
+        const tableItem = e.target.parentElement.parentElement
         if(target === "delBtn") {
-            e.target.parentElement.parentElement.remove()
+            tableItem.remove()
             notesCounter()
         }   
         if(target === "archBtn") {
-            oppositeTable.appendChild(e.target.parentElement.parentElement)
+            oppositeTable.appendChild(tableItem)
             notesCounter()
         }
         if(target === "editBtn") {
             showForm(editForm)
-            const div = e.target.parentElement.parentElement.childNodes
+            const div = tableItem.childNodes
             nameInputEdit.value = div[0].innerHTML
             categoryOptionsEdit.value = div[2].innerHTML
             noteContentEdit.value = div[3].innerHTML
@@ -71,28 +79,31 @@ function itemControls(e,table, oppositeTable) {
             notesCounter()
         }
 }
-// variables
-const showNoteFormBtn = document.querySelector("#showNoteForm")
-const formContainer = document.querySelector("#formContainer")
-const createNoteForm = document.querySelector("#createNoteForm")
-const editForm = document.querySelector("#editForm")
-const MainTable = document.querySelector("#main-table")
-const ArchiveTable = document.querySelector("#archive-table")
-const deleteAll = document.querySelectorAll(".deleteAll")
-const archiveAll = document.querySelectorAll(".archiveAll")
-// forms variables 
-const nameInputCreate = createNoteForm.querySelector("input[name=name")
-const noteContentCreate = createNoteForm.querySelector("textarea")
-const categoryOptionsCreate = createNoteForm.querySelector("select")
+// variables ===============
+    // tables
+    const MainTable = document.querySelector("#main-table")
+    const ArchiveTable = document.querySelector("#archive-table")
+    const countTable = document.querySelector("#count-table")
 
-const nameInputEdit = editForm.querySelector("input[name=name")
-const noteContentEdit = editForm.querySelector("textarea")
-const categoryOptionsEdit = editForm.querySelector("select")
-const editNoteBtn = editForm.querySelector("#editNote")
+    const deleteAll = document.querySelectorAll(".deleteAll")
+    const archiveAll = document.querySelectorAll(".archiveAll") 
+    // forms
+    const formContainer = document.querySelector("#formContainer")
+    const createNoteForm = document.querySelector("#createNoteForm")
+    const forms = document.querySelectorAll(".form")
+    // forms variables
+    const editForm = document.querySelector("#editForm")
+    const showNoteFormBtn = document.querySelector("#showNoteForm")
+    const nameInputCreate = createNoteForm.querySelector("input[name=name")
+    const noteContentCreate = createNoteForm.querySelector("textarea")
+    const categoryOptionsCreate = createNoteForm.querySelector("select")
 
-const createNoteBtn = document.querySelector("#createNote")
-const closeForm = document.querySelectorAll(".closeForm")
-const forms = document.querySelectorAll(".form")
+    const nameInputEdit = editForm.querySelector("input[name=name")
+    const noteContentEdit = editForm.querySelector("textarea")
+    const categoryOptionsEdit = editForm.querySelector("select")
+    const editNoteBtn = editForm.querySelector("#editNote")
+    const createNoteBtn = document.querySelector("#createNote")
+    const closeForm = document.querySelectorAll(".closeForm")
 // eventListeners
 showNoteFormBtn.addEventListener("click", ()=> showForm(createNoteForm))
 closeForm.forEach(btn=> {
@@ -101,23 +112,21 @@ closeForm.forEach(btn=> {
         btn.parentElement.classList.remove("active")
     })
 })
-function fastCreate(parent, element, content, className) {
-    const newElement = document.createElement(element)
-    newElement.innerHTML = content
-    if(className !== undefined) {
-        newElement.classList.add(className)
-    }
-    parent.appendChild(newElement)
-}
+
 createNoteForm.addEventListener("submit", e=> {
     e.preventDefault()
     const div = document.createElement("div")
     div.classList.add("table-item")
-    fastCreate(div, "p", nameInputCreate.value)
-    fastCreate(div, "p", new Date().toLocaleDateString())
-    fastCreate(div, "p", categoryOptionsCreate.value)
-    fastCreate(div, "p", noteContentCreate.value)
-    fastCreate(div, "p", noteContentCreate.value.match(/\d{2}[./-]\d{2}[./-]\d{4}/g))
+    const auxArray=[
+                    nameInputCreate.value, 
+                    new Date().toLocaleDateString(),
+                    categoryOptionsCreate.value,
+                    noteContentCreate.value,
+                    noteContentCreate.value.match(/\d{2}[./-]\d{2}[./-]\d{4}/g)
+                ]
+    auxArray.forEach(item=> {
+        fastCreate(div, "p", item)
+    })
     const controls = document.createElement("div")
     controls.classList.add("controls")
     fastCreate(controls, "button", "изменить", "editBtn")
